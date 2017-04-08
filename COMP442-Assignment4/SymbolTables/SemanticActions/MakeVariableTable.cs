93a7dcd09579d73fd237c416ad76b401fabc9f65
Michael B <netopya@netopyaplanet.com>
2017-04-08 11:15:10 -0400

    missing commit

--- COMP442-Assignment4/SymbolTables/SemanticActions/MakeVariableTable.cs
+++ COMP442-Assignment4/SymbolTables/SemanticActions/MakeVariableTable.cs
@@ -5,13 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using COMP442_Assignment4.Lexical;
 using COMP442_Assignment4.SymbolTables.SemanticRecords;
+using COMP442_Assignment4.CodeGeneration;
 
 namespace COMP442_Assignment4.SymbolTables.SemanticActions
 {
     // Create an entry for a variable declaration
     class MakeVariableTable : SemanticAction
     {
-        public override List<string> ExecuteSemanticAction(Stack<SemanticRecord> semanticRecordTable, Stack<SymbolTable> symbolTable, IToken lastToken, LinkedList<string> moonCode)
+        public override List<string> ExecuteSemanticAction(Stack<SemanticRecord> semanticRecordTable, Stack<SymbolTable> symbolTable, IToken lastToken, MoonCodeResult moonCode)
         {
             SymbolTable currentTable = symbolTable.Peek();
 
@@ -20,7 +21,7 @@ namespace COMP442_Assignment4.SymbolTables.SemanticActions
 
             Entry variableEntry = new VarParamEntry(currentTable, variableRecord.getVariable(), EntryKinds.variable);
 
-            moonCode.AddFirst(string.Format("{0} dw 0", variableEntry.getAddress()));
+            moonCode.AddLine("global", string.Format("{0} dw 0", variableEntry.getAddress()));
 
             return new List<string>();
         }
