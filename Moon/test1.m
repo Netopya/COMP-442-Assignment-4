f17ae89635c0c641f2355eaa881b3da953594d30
Michael B <netopya@netopyaplanet.com>
2017-04-09 10:56:12 -0400

    Updated test code

--- Moon/test1.m
+++ Moon/test1.m
@@ -1,47 +1,127 @@
 globals
-                program_foo_8 dw 0
-                program_bar_9 dw 0
-                const_program_13 dw 48
-                arithmExpr_program_14 dw 0
-                const_program_15 dw 2
-                arithmExpr_program_16 dw 0
-                const_program_18 dw 48
-                arithmExpr_program_20 dw 0
-program_7
+                program_input_313 dw 0
+                program_zero_314 dw 0
+                program_space_315 dw 0
+                const_program_317 dw 48
+                const_program_319 dw 32
+                const_program_322 dw 1
+                arithmExpr_program_323 dw 0
+                arithmExpr_program_328 dw 0
+                program_result_329 dw 0
+                const_program_332 dw 5
+                arithmExpr_program_333 dw 0
+                arithmExpr_program_336 dw 0
+                const_program_340 dw 5
+                arithmExpr_program_341 dw 0
+                const_program_343 dw 48
+                arithmExpr_program_344 dw 0
+                const_program_348 dw 5
+                arithmExpr_program_349 dw 0
+                arithmExpr_program_352 dw 0
+program_312
                 entry
                 
+                    lw r2, const_program_317(r0)
+                    sw program_zero_314(r0), r2
+                
+                
+                    lw r2, const_program_319(r0)
+                    sw program_space_315(r0), r2
+                
+                
                 getc r2
-                sw program_foo_8(r0), r2
+                sw program_input_313(r0), r2
             
                 
-                    lw r3, program_foo_8(r0)
-                    lw r4, const_program_13(r0)
-                    sub r2, r3, r4
-                    sw arithmExpr_program_14(r0), r2
-                
-                
-                    lw r3, arithmExpr_program_14(r0)
-                    lw r4, const_program_15(r0)
-                    and r2, r3, r4
-                    bz r2, zero_17
-                    addi r2, r0, 1
-                    sw arithmExpr_program_16(r0), r2
-                    j endop_17
-                    zero_17 sw arithmExpr_program_16(r0), r0
-                    endop_17
-                
-                
-                    lw r2, arithmExpr_program_16(r0)
-                    sw program_bar_9(r0), r2
-                
-                
-                    lw r3, const_program_18(r0)
-                    lw r4, program_bar_9(r0)
+                    lw r3, program_input_313(r0)
+                    lw r4, const_program_322(r0)
                     add r2, r3, r4
-                    sw arithmExpr_program_20(r0), r2
+                    sw arithmExpr_program_323(r0), r2
                 
                 
-                lw r2, arithmExpr_program_20(r0)
+                lw r2, arithmExpr_program_323(r0)
+                putc r2
+            
+                
+                lw r2, program_space_315(r0)
+                putc r2
+            
+                
+                    lw r3, program_input_313(r0)
+                    lw r4, program_zero_314(r0)
+                    sub r2, r3, r4
+                    sw arithmExpr_program_328(r0), r2
+                
+                
+                    lw r2, arithmExpr_program_328(r0)
+                    sw program_input_313(r0), r2
+                
+                
+                    lw r3, program_input_313(r0)
+                    lw r4, const_program_332(r0)
+                    ceq r2, r3, r4
+                    sw arithmExpr_program_333(r0), r2
+                
+                
+                    lw r2, arithmExpr_program_333(r0)
+                    sw program_result_329(r0), r2
+                
+                
+                    lw r3, program_result_329(r0)
+                    lw r4, program_zero_314(r0)
+                    add r2, r3, r4
+                    sw arithmExpr_program_336(r0), r2
+                
+                
+                lw r2, arithmExpr_program_336(r0)
+                putc r2
+            
+                
+                lw r2, program_space_315(r0)
+                putc r2
+            
+                
+                    lw r3, program_input_313(r0)
+                    lw r4, const_program_340(r0)
+                    cgt r2, r3, r4
+                    sw arithmExpr_program_341(r0), r2
+                
+                
+                    lw r2, arithmExpr_program_341(r0)
+                    sw program_result_329(r0), r2
+                
+                
+                    lw r3, program_result_329(r0)
+                    lw r4, const_program_343(r0)
+                    add r2, r3, r4
+                    sw arithmExpr_program_344(r0), r2
+                
+                
+                lw r2, arithmExpr_program_344(r0)
+                putc r2
+            
+                
+                lw r2, program_space_315(r0)
+                putc r2
+            
+                
+                    lw r3, program_input_313(r0)
+                    lw r4, const_program_348(r0)
+                    clt r2, r3, r4
+                    sw arithmExpr_program_349(r0), r2
+                
+                
+                    lw r2, arithmExpr_program_349(r0)
+                    sw program_result_329(r0), r2
+                
+                
+                    lw r3, program_result_329(r0)
+                    lw r4, program_zero_314(r0)
+                    add r2, r3, r4
+                    sw arithmExpr_program_352(r0), r2
+                
+                
+                lw r2, arithmExpr_program_352(r0)
                 putc r2
             
                 hlt
